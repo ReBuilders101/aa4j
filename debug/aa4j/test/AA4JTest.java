60d1c268d69bb6c69d8957fb8a73b72955f2521b
ReBuilders101 <rebuilders101@gmail.com>
2021-11-11 21:10:00 +0100

    Updated debug to be error-free

--- debug/aa4j/test/AA4JTest.java
+++ debug/aa4j/test/AA4JTest.java
@@ -1,36 +1,42 @@
 package aa4j.test;
 
+import aa4j.task.TaskAccess;
 import aa4j.task.TaskOf;
-import static aa4j.AA4JStatic.*;
+import aa4j.task.Tasks;
 
 import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionException;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 @SuppressWarnings("javadoc")
 public class AA4JTest {
 
 	public static void main(String[] args) throws InterruptedException, ExecutionException, CancellationException, IllegalStateException, IOException {
-
-		CompletableFuture<String> cpf = CompletableFuture.completedFuture("asdf");
-		cpf.whenComplete((a,b) -> System.out.println(a + " - " + b));
-		System.out.println(cpf.thenApply(str -> str.length()).get());
+		TaskAccess<String> taskAccess = Tasks.create(null);
+		var task1 = taskAccess.taskOf();
+		var task2 = Tasks.chain(task1, AA4JTest::parseIntAsync);
+		var task2Alt = Tasks.map(task1, Integer::parseInt);
+		
+		taskAccess.tcsOf().succeed("1234asdf");
+//		System.out.println(task1.cancel());
+//		System.out.println(taskAccess.tcsOf().succeed("1234"));
+//		taskAccess.tcs().fail(new CancellationException("Does not actually cancel the task!"));
+		
+		Tasks.report("task1", task1);
+		Tasks.report("task2", task2);
+		Tasks.report("task2Alt", task2Alt);
+		
 	}
-
 	
-	TaskOf<Integer> foo() {
-		TaskOf<String> future = ((TaskOf<String>) null); 
-		
-		future.getResult(TO_UNCHECKED);
-		
-		return await(future, str -> {
-			return Integer.valueOf(str);
-		});
+	static TaskOf<Integer> parseIntAsync(String string) {
+		return Tasks.success(Integer.valueOf(string));
+	}
+	
+	static CompletableFuture<Integer> parseInt(String string) {
+		//return CompletableFuture.failedFuture(new CancellationException());
+		return new CompletableFuture<Integer>().completeOnTimeout(Integer.valueOf(string), 50, TimeUnit.MILLISECONDS);
 	}
 	
 }
--- debug/aa4j/test/Examples.java
+++ debug/aa4j/test/Examples.java
@@ -6,7 +6,7 @@ import java.nio.channels.SocketChannel;
 import java.util.Objects;
 import java.util.concurrent.TimeoutException;
 
-import aa4j.AA4JStatic;
+import aa4j.AA4J;
 import aa4j.task.TaskAccess;
 import aa4j.task.TaskOf;
 import aa4j.task.Tasks;
@@ -17,7 +17,7 @@ public class Examples {
 		Objects.requireNonNull(address, "'address' parameter cannot be null");
 		if(timeout < 0) throw new IllegalArgumentException("'timeout' parameter cannot be negative"); 
 		
-		TaskAccess<Integer> res = Tasks.manualBlocking();
+		TaskAccess<Integer> res = Tasks.create();
 		new Thread(() -> {
 			try {
 				long start = System.currentTimeMillis();
@@ -39,14 +39,14 @@ public class Examples {
 	}
 	
 	public static TaskOf<Object> receiveData(SocketChannel channel) {
-		
+		return null;
 	}
 	
 	public static TaskOf<Object> foo() {
 		//Sync code generates address
 		InetAddress inetaddress = null;
 
-		return AA4JStatic
+		return AA4J
 				.sync(() -> inetaddress)
 				.await(address -> connectChannel(address))
 				.await(channel -> receiveData(channel))
@@ -57,9 +57,8 @@ public class Examples {
 		//Sync code generates address
 		InetAddress inetaddress = null;
 		
-		return AA4JStatic
-			   .sync(() -> inetaddress)
-			   .await(address -> connectChannel(address))
+		return AA4J
+			   .await(connectChannel(inetaddress))
 			   .await(channel -> receiveData(channel))
 			   .task();
 	}
